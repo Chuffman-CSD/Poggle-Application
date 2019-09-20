1bf8121a0c3b7d34f6de8216c023134a3d0872cc
Chuffman-CSD <chuffman@stcharlessd.org>
2019-09-20 13:25:21 -0500

    deleted:    Poggle Application Project/PoggleClient.py 	renamed:    Poggle Application Project/Client.py -> Poggle Application Project/Server Connections/PoggleClient.py 	renamed:    Poggle Application Project/PoggleServer.py -> Poggle Application Project/Server Connections/PoggleServer.py 	renamed:    Poggle Application Project/PoggleBody.py -> Poggle Application Project/Start Up/PoggleBody.py 	renamed:    Poggle Application Project/PoggleLogin.py -> Poggle Application Project/Start Up/PoggleLogin.py

--- Poggle Application Project/Server Connections/PoggleClient.py
+++ Poggle Application Project/Server Connections/PoggleClient.py
@@ -5,20 +5,20 @@
 import socket
 import sys
 
-host = ''
+host = '127.0.0.1'
 port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((host,port))
+    s.connect((host,port))
 except socket.error as e:
     print (str(e))
-s.listen(5)
 
 def threaded_client(conn):
-    conn.send(str.endcode('welcome, type a message'))
+    conn.send(str.encode('welcome, type a message'))
 
     while True:
         data = conn.recv(2048)
         reply = 'Server Output: ' + data.decode('t-f8')
         
+threaded_client(s)
